e3f37737d0446295aa7fd2308b9a2c0abd6c94e0
Keyop4 <vhudson@rogers.com>
2022-07-27 11:33:47 -0400

    Add comments

--- src/services/log.js
+++ src/services/log.js
@@ -1,6 +1,27 @@
 const db = require('./database.js');
 const lineByLine = require('n-readlines');
 
+//This function reads a file line by line and ultimately calls another
+//function to insert each line into the log table
+function readLogFile(requestQuery){
+    const {file} = requestQuery
+    const liner = new lineByLine(process.env.FILE_DIRECTORY+file); 
+    let line;   
+    
+    while (line = liner.next()) {
+        trimmedLine = line.toString('ascii').trim();
+        insertLogEvent(trimmedLine,file);
+    }
+
+}
+
+//This function inserts a record into the log table
+function insertLogEvent(event, fileName){
+    db.run(`INSERT INTO log (event, file) VALUES ("${event}","${fileName}")`)
+}
+
+//This function queries the db and returns the appropriate
+//records based on the search criteria 
 function getLogs(requestQuery){
     const {file, keyword, limit} = requestQuery
     let keywordSearch = "";
@@ -20,22 +41,6 @@ function getLogs(requestQuery){
             resolve(rows);
         });
     })    
-}
-
-function readLogFile(requestQuery){
-    const {file} = requestQuery
-    const liner = new lineByLine(process.env.FILE_DIRECTORY+file); 
-    let line;   
-    
-    while (line = liner.next()) {
-        trimmedLine = line.toString('ascii').trim();
-        insertLogEvent(trimmedLine,file);
-    }
-
-}
-
-function insertLogEvent(event, fileName){
-    db.run(`INSERT INTO log (event, file) VALUES ("${event}","${fileName}")`)
 }
 
 module.exports = {
